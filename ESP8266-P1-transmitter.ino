ea65bf40840d5549c06e0ea3d57662d27a775780
Maarten van Ingen <maarten.vaningen@surfsara.nl>
2017-10-20 09:56:47 +0200

    - Construct a full telegram before parsing  - Parsing still sucks so must be revised.

--- ESP8266-P1-transmitter.ino
+++ ESP8266-P1-transmitter.ino
@@ -1,7 +1,9 @@
 /*
+Disclaimer: This is a work in progress and might or might not work.
+
 My smart meter:
 
-DSMR 4.2
+DSMR 5.0
 115200 8N1:
 Baudrate = 115200
 Data bits = 8
@@ -13,7 +15,9 @@ inverted 5V uart
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiUdp.h>
-#include <PubSubClient.h>
+//#include <PubSubClient.h>
+#define MAX_SRV_CLIENTS 1
+
 
 //put settings in config.h in the same dir as the sketch or inline:
 #include "config.h"
@@ -23,43 +27,32 @@ const char* password = "Your pass";
 IPAddress mqttserver(127,0,0,1); // fill in your MQTT server
 IPAddress influxdb(127,0,0,1); //influxdb server ip
 unsigned int influxport = 8888;
+String deviceName = "SmartMeter";
+String testTelegram = "";
 */
 
-WiFiClient wclient;
-PubSubClient client(wclient, mqttserver);
+WiFiServer server(23);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
+//WiFiClient wclient;
+// PubSubClient client(wclient, mqttserver);
 int incomingByte = 0;
-String inputString;
+String telegram;
 
 void setup() {
 	Serial.begin (115200);
 	WiFi.begin (ssid,password);
-	Serial.println("");
 
 	// Wait for connection
 	while (WiFi.status() != WL_CONNECTED) {
 		delay (500);
-		Serial.print(".");
 	}
-	Serial.println("");
-	Serial.print("Connected to ");
-	Serial.println(ssid);
-	Serial.print("IP address: ");
-	Serial.println(WiFi.localIP());
+
 }
 
 void loop () {
-   if (WiFi.status() == WL_CONNECTED) {
-      if (!client.connected()) {
-         if (client.connect("ESP8266-slimmemeter")) {
-            Serial.println("mqtt connected");
-            client.publish("/sensor/p1/startup", "Hello from the ESP8266 connected to the smart meter");
-         }
-    }
-    if (client.connected()) {
       CheckSerial();
-      client.loop();
-    }
-  }
+      delay(1000);
 }
 
 void SendInflux(String message)
@@ -70,47 +63,126 @@ void SendInflux(String message)
    UDP.endPacket();
 }
 
+void SendDebug(String message)
+{
+   WiFiUDP UDP;
+   UDP.beginPacket(influxdb, influxport+1);
+   UDP.print(message);
+   UDP.endPacket();
+}
 void CheckSerial(){
-   while (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    char inChar = (char)incomingByte;
-    inputString += inChar;
+      bool beginOfTelegramFound = false;
+      bool endOfTelegramFound = false;
+      bool fullTelegramFetched = false;
+      char sbuf[2048];
+      int i = 0;
+      char crc[4];
+      String rawData = "";
+
+  // Read a full telegram
+  while ( !fullTelegramFetched )
+  {
+    if( Serial.available() )
+    {
+      char readChar = (char)Serial.read();
+      if ( readChar == '/' )
+      {
+        beginOfTelegramFound = true;
+        endOfTelegramFound = false;
+        telegram = "!";
+        continue;
+      }
+      if ( readChar == '!' )
+      {
+        endOfTelegramFound = true;
+        i = 0;
+        continue;
+      }
+      if ( beginOfTelegramFound && !endOfTelegramFound )
+      {
+        telegram += readChar;
+        continue;
+      }
+      if (endOfTelegramFound && i < 4)
+      {
+        crc[i] = readChar;
+        i++;
+        continue;
+      }
+      if (endOfTelegramFound && i >= 4)
+      {
+         fullTelegramFetched = true;
+      }
+    }
+  }
+
+   SendDebug(telegram);
+   
+
+   // Okay, no more data, but we haven't fetched a full frame. As we are missing data for sure, break.
+   if ( !fullTelegramFetched )
+   {
+    return;
    }
-   //each telegrams ends with an exclammation mark
-   if (inputString.endsWith("!")) {
-      //Publish entire unparsed string to MQTT for external parsing or debugging
-      client.publish("/sensor/p1/raw",inputString);
+
+      String udpString = "Power,device=" + deviceName + " debug=1";
 
       // Extract substrings/values
-      int T1_pos = inputString.indexOf("1-0:1.8.1", 0);
-      String T1 = inputString.substring(T1_pos + 10, T1_pos + 20);
-      client.publish("/sensor/p1/T1",T1);
+      // Meter Reading electricity delivered to client (Tariff 1) in 0,001 kWh
+      int T1_pos = telegram.indexOf("1-0:1.8.1", 0);
+      if (T1_pos >= 0)
+      {
+        String T1 = telegram.substring(T1_pos + 10, T1_pos + 20);
+        udpString += ",T1=" + T1;
+      }
 
-      int T2_pos = inputString.indexOf("1-0:1.8.2", T1_pos + 1);
-      String T2 = inputString.substring(T2_pos + 10, T2_pos + 20);
-      client.publish("/sensor/p1/T2",T2);
-      
-      int T7_pos = inputString.indexOf("1-0:2.8.1", T1_pos + 1);
-      String T7 = inputString.substring(T7_pos + 10, T7_pos + 20);
-      client.publish("/sensor/p1/T7",T7);
+      // Meter Reading electricity delivered to client (Tariff 2) in 0,001 kWh
+      int T2_pos = telegram.indexOf("1-0:1.8.2", T1_pos + 1);
+      if (T2_pos >= 0)
+      {
+        String T2 = telegram.substring(T2_pos + 10, T2_pos + 20);
+        udpString += ",T2=" + T2;
+      }
 
-      int T8_pos = inputString.indexOf("1-0:2.8.2", T7_pos + 1);
-      String T8 = inputString.substring(T8_pos + 10, T8_pos + 20);
-      client.publish("/sensor/p1/T8",T8);
+      // Meter Reading electricity delivered by client (Tariff 1) in 0,001 kWh
+      int T7_pos = telegram.indexOf("1-0:2.8.1", T1_pos + 1);
+      if (T7_pos >= 0)
+      {
+        String T7 = telegram.substring(T7_pos + 10, T7_pos + 20);
+        udpString += ",T7=" + T7;
+      }
 
-      int P1_pos = inputString.indexOf("1-0:1.7.0", T8_pos + 1);
-      String P1 = inputString.substring(P1_pos + 10, P1_pos + 16);
-      client.publish("/sensor/p1/P1",P1);
+      // Meter Reading electricity delivered by client (Tariff 2) in 0,001 kWh
+      int T8_pos = telegram.indexOf("1-0:2.8.2", T7_pos + 1);
+      if (T8_pos >= 0)
+      {
+        String T8 = telegram.substring(T8_pos + 10, T8_pos + 20);
+        udpString += ",T8=" + T8;
+      }
 
-      int P2_pos = inputString.indexOf("1-0:2.7.0", P1_pos + 1);
-      String P2 = inputString.substring(P2_pos + 10, P2_pos + 16);
-      client.publish("/sensor/p1/P2",P2);
+      // Actual electricity power delivered (+P) in 1 Watt reso- lution
+      int P1_pos = telegram.indexOf("1-0:1.7.0", T8_pos + 1);
+      if (P1_pos >= 0)
+      {
+        String P1 = telegram.substring(P1_pos + 10, P1_pos + 16);
+        udpString += ",P1=" + P1;
+      }
 
-      int G1_pos = inputString.indexOf("0-1:24.2.1", P2_pos + 1);
-      String G1 = inputString.substring(G1_pos + 26, G1_pos + 35);
-      client.publish("/sensor/p1/G1",G1); 
-      SendInflux("slimme_meter T1=" + T1 + ",T2=" + T2 + ",T7=" + T7 + ",T8=" + T8 + ",P1=" + P1 + ",P2=" + P2 + ",G1=" + G1);
+      // Actual electricity power received (-P) in 1 Watt resolution
+      int P2_pos = telegram.indexOf("1-0:2.7.0", P1_pos + 1);
+      if (P2_pos >= 0)
+      {
+        String P2 = telegram.substring(P2_pos + 10, P2_pos + 16);
+        udpString += ",P2=" + P2;
+      }
 
-      inputString = "";
-   }
+      // Last 5-minute value (temperature con- verted), gas deliv- ered to client in m3, including decimal values and capture time
+      int G1_pos = telegram.indexOf("0-1:24.2.1", P2_pos + 1);
+      if (G1_pos >= 0)
+      {
+        String G1 = telegram.substring(G1_pos + 26, G1_pos + 35);
+        udpString += ",G1=" + G1;
+      }
+      //SendInflux("Power,device=" + deviceName + " T1=" + T1 + ",T2=" + T2 + ",T7=" + T7 + ",T8=" + T8 + ",P1=" + P1 + ",P2=" + P2 + ",G1=" + G1);
+      SendInflux (udpString);
 }
